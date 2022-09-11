4937e7fa9a0816908da096b0a80c28d3fb04baf0
Joshua <joshuaamaral05@gmail.com>
2022-09-11 13:25:54 -0800

    Modificações Java e HTML

---
+++ js/loginScript.js
@@ -0,0 +1,14 @@
+function funcaoLogin() {
+
+    let login = document.getElementsByClassName("dadosLogin")
+
+    if (login.length > 0) {
+        alert (login[0].value);
+    }
+
+
+//     let login = document.getElementById(dadosLogin).Value
+
+// alert(login)
+// console.log(login)
+}
--- js/script.js
+++ js/script.js
@@ -1,12 +1,74 @@
+// JQUERY--TOGGLE FUNCTION
+
+$(document).ready(function() {
+
+   
+   
+
+
+
+   let slideAtual = 1;
+   let listaSlide = ["banner-jogos", "banner-amigos", "banner-chuva"]
+                           // 0               1               2
+
+       setInterval(mudarSlides, 1000)
+
+       function mudarSlides() {
+      
+      console.log("Slide atual é:", slideAtual);
+
+            // Remover o Slide anterior ao atual
+      if (slideAtual > 0 ) {
+         $("#carrosel").removeClass(listaSlide[slideAtual - 1])
+      }  else if (slideAtual == 0) {
+
+         $("#carrosel").removeClass(listaSlide[(listaSlide.length) - 1  ])
+      }
+            // Adicionando Slides
+         $("#carrosel").addClass(listaSlide[slideAtual]);
+   
+         // Indica o slide atual, qual vai ser
+         slideAtual++
+
+         if (slideAtual > (listaSlide.length) - 1) {
+            slideAtual = 0;
+         }
+      }
+   
+   
+   $("#bars").click(function() {
+
+      $("#menu").toggleClass("menu-ativo");
+   })
+
+})
+   //   if (  $("menu").hasClass("menu-ativo")   ) {
+   //       $("menu").removeClass("menu-ativo")
+   //   } else {
+      //       $("menu").addClass(menu-ativo)
+      //   }
+      
+
+  
+
+
+
 function mostrarMenu() {
 
 
    let menu = document.getElementById("menu")
 
-    { if (getComputedStyle(menu).display == 'none')
+     if (getComputedStyle(menu).display == 'none') {
         menu.style.display = 'flex'; 
      } else {
         menu.style.display = 'none'; }
      }
 
 
+function cadastrarNovoEmail() {
+
+   let email = document.getElementById("campo-email").value
+   
+   alert("E-mail cadastrado com sucesso")
+   console.log(email)
+}
